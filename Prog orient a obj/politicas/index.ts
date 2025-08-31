5dc688515d8ce29378e5e08a52917fc7b25a52ea
Enzo Pitana <enzopitana@gmail.com>
2025-08-31 17:39:13 -0300

    Create index.ts

---
+++ Prog orient a obj/politicas/index.ts
@@ -0,0 +1,5 @@
+export * from './IPoliticaPrestamo';
+export * from './PoliticaDocente';
+export * from './PoliticaEstricta';
+export * from './PoliticaEstudiante';
+export * from './PoliticaFlexible';
