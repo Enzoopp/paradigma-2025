987e39ac5190a2f4342bc148d58ab6096699be38
Enzo Pitana <enzopitana@gmail.com>
2025-08-31 15:00:45 -0300

    Create EventoBiblioteca.ts

---
+++ Prog orient a obj/core/EventoBiblioteca.ts
@@ -0,0 +1,48 @@
+import { SocioRegular } from "../usuarios/SocioRegular";
+
+/**
+ * se maneja todos los eventos que organizamos en la biblioteca
+ */
+export class EventoBiblioteca {
+  private _participantes: SocioRegular[] = [];
+
+  constructor(
+    private _nombre: string,
+    private _fecha: Date,
+    private _descripcion: string
+  ) {}
+
+  get nombre(): string {
+    return this._nombre;
+  }
+
+  get fecha(): Date {
+    return this._fecha;
+  }
+
+  get descripcion(): string {
+    return this._descripcion;
+  }
+
+  get participantes(): SocioRegular[] {
+    return this._participantes;
+  }
+
+  // Inscribimos gente a los eventos
+  agregarParticipante(socio: SocioRegular): void {
+    if (!this._participantes.includes(socio)) {
+      this._participantes.push(socio);
+      console.log(`${socio.nombreCompleto} ya esta anotado/a en "${this.nombre}"`);
+    } else {
+      console.log(`${socio.nombreCompleto} ya está inscrito/a en este evento`);
+    }
+  }
+
+  // Mandamos avisos a todos los participantes
+  notificarParticipantes(mensaje: string): void {
+    console.log(`\nAviso importante sobre "${this.nombre}":`);
+    this._participantes.forEach(socio => {
+      console.log(`  ${socio.nombreCompleto}: ${mensaje}`);
+    });
+  }
+}
